6c3e02099211a2797c8b897471efb1514c6d202c
calebcyr <calebcyr41@gmail.com>
2022-02-12 16:02:52 +0000

    file upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from './components/Header'
+import Main from './components/Main'
+import './css/App.css'
+
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
